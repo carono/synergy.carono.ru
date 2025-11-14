3766b54fba4d77be7cd0b4ec9bbc83687b4a93a3
Касьянов <info@carono.ru>
2025-11-15 00:56:55 +0500

    Персонализируем данные, убираем лишнее, добавляем перевод

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -55,12 +55,13 @@ class LoginForm extends Model
 
     /**
      * Logs in a user using the provided username and password.
+     *
      * @return bool whether the user is logged in successfully
      */
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
         return false;
     }
@@ -77,5 +78,14 @@ class LoginForm extends Model
         }
 
         return $this->_user;
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'username' => 'Логин',
+            'password' => 'Пароль',
+            'rememberMe' => 'Запомнить меня'
+        ];
     }
 }
