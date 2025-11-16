8a29961f8877af71f5845f16f6695aa4ad49cdc1
Касьянов <info@carono.ru>
2025-11-16 15:30:45 +0500

    кейс 2

--- modules/first/case2/Cat.php
+++ modules/first/case2/Cat.php
@@ -4,12 +4,6 @@ namespace app\modules\first\case2;
 
 class Cat extends Animal
 {
-    // Конструктор производного класса
-    public function __construct($name, $food)
-    {
-        parent::__construct($name, $food); // Вызов конструктора базового класса
-    }
-
     // Переопределение метода speak
     public function speak()
     {
